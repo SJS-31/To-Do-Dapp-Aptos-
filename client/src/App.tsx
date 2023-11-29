b316b108a38db9481f854fb8900aa0ac9ddcabe0
SJS-31 <rajswapnil31@gmail.com>
2023-11-29 20:10:12 +0530

    Front End

---
+++ client/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Col, Layout, Row } from 'antd';
+
+function App() {
+  return (
+    <>
+      <Layout>
+        <Row align="middle">
+          <Col span={10} offset={2}>
+            <h1>Our todolist</h1>
+          </Col>
+          <Col span={12} style={{ textAlign: "right", paddingRight: "200px" }}>
+            <h1>Connect Wallet</h1>
+          </Col>
+        </Row>
+      </Layout>
+    </>
+  );
+}
+
+export default App;
