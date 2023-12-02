1b8bdfb74d1cb1db441508ba97a3fe00c44d147c
SJS-31 <rajswapnil31@gmail.com>
2023-12-02 16:43:29 +0530

    Fetching Data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import { Col, Layout, Row } from "antd";
+import { Col, Layout, Row, Button } from "antd";
 import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
 import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
+import { Provider, Network } from "aptos";
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
+
+const provider = new Provider(Network.DEVNET);
 
 function App() {
+  const { account } = useWallet();
+  const [accountHasList, setAccountHasList] = useState<boolean>(false);
+
+  const fetchList = async () => {
+    if (!account) return [];
+    // change this to be your module account address
+    const moduleAddress =
+      "f2d288a5567522fb2f22cf14d45a376f2a75fb5c48f95c3affaa257b1acaaf65";
+      
+    try {
+      const TodoListResource = await provider.getAccountResource(
+        account.address,
+        `${moduleAddress}::todolist::TodoList`
+      );
+      setAccountHasList(true);
+    } catch (e: any) {
+      setAccountHasList(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, [account?.address]);
+
   return (
     <>
       <Layout>
@@ -13,17 +41,24 @@ function App() {
             <h1>Our todolist</h1>
           </Col>
           <Col span={12} style={{ textAlign: "right", paddingRight: "200px" }}>
-            <Col
-              span={12}
-              style={{ textAlign: "right", paddingRight: "200px" }}
-            >
-              <WalletSelector />
-            </Col>
+            <WalletSelector />
           </Col>
         </Row>
       </Layout>
+      {!accountHasList && (
+        <Row gutter={[0, 32]} style={{ marginTop: "2rem" }}>
+          <Col span={8} offset={8}>
+            <Button block type="primary" style={{ height: "40px", backgroundColor: "#3f67ff" }}>
+              Add new list
+            </Button>
+          </Col>
+        </Row>
+      )}
     </>
   );
 }
 
 export default App;
+function fetchList() {
+  throw new Error("Function not implemented.");
+}
